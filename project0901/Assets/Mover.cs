ab14339de5902fa158226b28abeb8aa77f221ea0
kimju0714 <rlaehdehd2001@gmail.com>
2022-09-01 15:11:29 +0900

    섹션 4-2 실습코드

---
+++ project0901/Assets/Mover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Vector3 move = new Vector3(-5, 5, -5);
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Move();
+        }
+    }
+
+
+
+    void Move()
+    {
+        transform.Translate(move * Time.deltaTime);
+    }
+}
